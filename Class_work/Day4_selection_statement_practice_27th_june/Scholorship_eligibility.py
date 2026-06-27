c351f900aaec24459127df2c11c3c0aefcd63c03
Tanuja24-ipec <24cse413@gmail.com>
2026-06-27 09:41:09 +0530

    Program to  check a student is eligible for scholarship or not

---
+++ Class_work/Day4_selection_statement_practice_27th_june/Scholorship_eligibility.py
@@ -0,0 +1,30 @@
+""" -----------------------------Scholarship Eligibility-------------------------------  
+Problem Statement A university provides a scholarship only to students who score 90 or above. 
+
+Write a Python program to accept a student's percentage and determine whether the student is eligible. 
+• If percentage is 90 or above, display: Scholarship Approved 
+• Otherwise display: Scholarship Not Approved 
+
+Sample Input 92 
+Sample Output Scholarship Approved """
+
+
+#--------------------------Coding Section----------------------------------------
+
+
+#Taking input from the user
+percentage = float(input("Enter the student's percentage: "))
+
+#Validating the percentage
+if percentage >= 90:
+    print("Scholarship Approved")
+else:
+    print("Scholarship Not Approved")
+
+
+#---------------------------Output section----------------------------------------
+
+""" 
+Sample input:
+Enter the student's percentage: 92
+Sample output: Scholarship Approved"""
