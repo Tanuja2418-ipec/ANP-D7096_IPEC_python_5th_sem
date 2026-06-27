4e0cf01e7f3dc4094b1557ac024d225f10028737
Tanuja24-ipec <24cse413@gmail.com>
2026-06-27 09:47:36 +0530

    program to evaluate the delivery charge based on the weight

---
+++ Class_work/Day4_selection_statement_practice_27th_june/Courier_delivery_charge.py
@@ -0,0 +1,39 @@
+"""-------------------------------------Courier Delivery Charge----------------------------
+ Problem Statement A courier company calculates delivery charges based on the package weight. 
+ • Weight up to 2 kg → ₹50  
+ • Weight greater than 2 kg and up to 5 kg → ₹100  
+ • Weight greater than 5 kg → ₹180  
+ 
+ Write a Python program to display the delivery charge. 
+ 
+ Sample Input 4 
+ Sample Output Delivery Charge = ₹100 """
+
+#--------------------------Coding Section-----------------------------------   
+
+#taking the input from the user
+weight = float(input("Enter the package weight in kg: "))
+
+#Calculating the delivery chage based on the weight
+
+if weight <= 2:
+    delivery_charge = 50
+elif weight > 2 and weight <= 5:
+    delivery_charge = 100
+else:
+    delivery_charge = 180
+
+print("Delivery Charge = Rs", delivery_charge)
+
+
+#-----------------------------Output Section-----------------------------------
+
+"""   
+Sample input: 6
+Sample output: Delivery Charge = Rs 180
+
+Sample input: 1
+Sample output: Delivery Charge = Rs 50
+
+Sample input: 4
+Sample output: Delivery Charge = Rs 100"""
