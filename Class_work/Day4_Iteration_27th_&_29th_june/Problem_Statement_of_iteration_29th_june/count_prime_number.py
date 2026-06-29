25ea04fcb793fa413bd1db02110c5dd62cef1b82
Tanuja24-ipec <24cse413@gmail.com>
2026-06-29 10:50:49 +0530

    Program to fing the prime number

---
+++ Class_work/Day4_Iteration_27th_&_29th_june/Problem_Statement_of_iteration_29th_june/count_prime_number.py
@@ -0,0 +1,30 @@
+"""Count Prime Numbers in a Range
+Problem Statement
+Accept two integers representing the starting and ending values of a range.
+Display all prime numbers within the range and finally display the total number of prime numbers found."""
+
+
+#--------------------------coding section-----------------------------
+
+
+#taking input from the user
+
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
+
+count = 0
+
+for num in range(start, end + 1):
+    if num > 1:
+        is_prime = True
+
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            print(num)
+            count += 1
+
+print("Total Prime Numbers:", count)
