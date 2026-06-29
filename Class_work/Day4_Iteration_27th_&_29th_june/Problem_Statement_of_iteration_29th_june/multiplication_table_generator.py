3d0e6dc08210076f7fce2f664483e46af1ac8c01
Tanuja24-ipec <24cse413@gmail.com>
2026-06-29 10:40:20 +0530

    Program to generate multiplication table

---
+++ Class_work/Day4_Iteration_27th_&_29th_june/Problem_Statement_of_iteration_29th_june/multiplication_table_generator.py
@@ -0,0 +1,16 @@
+"""Multiplication Table Generator
+Problem Statement
+Write a Python program that accepts a number from the user and displays its multiplication table up to 20.
+Sample Output
+Enter Number: 8 8 x 1 = 8 8 x 2 = 16 ... 8 x 20 = 160"""
+
+
+#---------------------------coding section-------------------------------
+
+#taking input of a number from the user
+
+num = int(input("Enter a number : "))
+
+
+for i in range(1,21):
+    print(num, "x", i, "=", num * i)
