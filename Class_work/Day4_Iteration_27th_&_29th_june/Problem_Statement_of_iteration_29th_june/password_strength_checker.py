24c6e4c7fdfec4223691a2c18dc1074bbeccca98
Tanuja24-ipec <24cse413@gmail.com>
2026-06-29 10:20:23 +0530

    Program to check the password strength

---
+++ Class_work/Day4_Iteration_27th_&_29th_june/Problem_Statement_of_iteration_29th_june/password_strength_checker.py
@@ -0,0 +1,16 @@
+"""Password Strength Checker
+Problem Statement
+A website requires users to create a password having at least 8 characters.
+Keep asking the user to enter a password until the entered password satisfies the minimum length requirement.
+Sample Output
+Enter Password: hello Password too short. Enter Password: python@123 Password Accepted."""
+
+
+#---------------------Coding Section---------------------------------
+
+password = input("Enter the Password : ")
+
+while len(password) < 8:
+    print("Password too Short.")
+    password = input("Enter the Password : ")
+print("Password Accepted.")    
