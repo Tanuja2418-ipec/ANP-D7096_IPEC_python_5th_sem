7ef971e9d43ca976bd6b960d245b92b9b1cd9fba
Tanuja24-ipec <24cse413@gmail.com>
2026-07-08 10:51:22 +0530

    module to numeric calculation

---
+++ Class_work/Day10_module_8th_july/numeric_calculation.py
@@ -0,0 +1,19 @@
+#function to calculate addition of two numbers
+def calculate_addition(num1,num2):
+    return (num1+num2)
+
+#function to calculate difference of two numbers
+def calculate_difference(num1,num2):
+    return (num1-num2)
+
+#function to calculate product of two numbers
+def calculate_product(num1,num2):
+    return (num1*num2)
+
+#function to calculate division of two numbers
+def calculate_division(num1,num2):
+    return (num1/num2)
+
+#function to calculate remainder of two numbers
+def calculate_remainder(num1,num2):
+    return (num1%num2)
